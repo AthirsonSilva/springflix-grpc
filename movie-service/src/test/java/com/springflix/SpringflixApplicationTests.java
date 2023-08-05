8832160e458ff1185266edbcafd29ed92debd002
Athirson Silva <Athirsonarceus@gmail.com>
2023-08-05 20:29:46 -0300

    multi module maven project

---
+++ movie-service/src/test/java/com/springflix/SpringflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ proto/src/main/java/com/springflix/SpringflixApplication.java
@@ -0,0 +1,9 @@
+package com.springflix;
+
+public class SpringflixApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
